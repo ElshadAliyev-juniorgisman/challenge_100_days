71c03a7d0c6697e8de8e0139c04335a7eecd6e56
ElshadAliyev-juniorgisman <161311378+ElshadAliyev-juniorgisman@users.noreply.github.com>
2024-02-26 14:52:59 +0400

    day_1

---
+++ yaş hesablama.py
@@ -0,0 +1,6 @@
+print("Salam yaş hesablama sisteminə xoş gəlmisiz.")
+ad = input("Zəhmət olmasa adınızı qeyd edin: ")
+ya = input("Zəhmət olmasa doğulduğnuz ili qeyd edin: ")
+il = 2024 - int(ya)
+yh = il*52
+print(f"{ad}  {ya}  {il}  yaşın var \n{yh} həftə yaşamısan  ")
